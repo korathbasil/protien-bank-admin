d0dc8d1d4eb5512187740009ca3635e31ab18292
Bazil Korath <korathbasil@live.com>
2022-02-01 00:25:24 +0530

    add basic routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 
-// import { Home, Login } from "pages";
+import { Home, Login } from "../pages";
 
 function App() {
-  return <div className="app">Protien Bank admin</div>;
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+      ;
+    </BrowserRouter>
+  );
 }
 
 export default App;
